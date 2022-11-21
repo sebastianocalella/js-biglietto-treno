809129ba5d2e6464c957df48418039c4c0fd46fa
sebastianocalella <sebastianocalella97@gmail.com>
2022-11-21 14:41:22 +0100

    fixies

--- script.js
+++ script.js
@@ -5,12 +5,16 @@ console.log (distance);
 
 let price = (distance * 0.21);
 
-let finalPrice = price.toFixed(2);
+let finalPrice;
 
 if (age < 18){
-    finalPrice = ((price / 100) * 80)
+    finalPrice = ((price / 100) * 80);
 } else if (age > 65){
-    finalPrice = ((price / 100) * 60)
+    finalPrice = ((price / 100) * 60);
+} else {
+    finalPrice = price;
 }
 
-document.getElementById('price').innerHTML = ' ' + finalPrice + '€';
+finalPrice = finalPrice.toFixed(2);
+
+document.getElementById('price').innerHTML = ' ' + finalPrice + ' €';
