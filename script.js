7218bf93334e56d035daaaf1328430ed8196981f
sebastianocalella <sebastianocalella97@gmail.com>
2022-11-21 14:49:35 +0100

    addictional feature

--- script.js
+++ script.js
@@ -7,10 +7,16 @@ let price = (distance * 0.21);
 
 let finalPrice;
 
+let sale;
+
 if (age < 18){
     finalPrice = ((price / 100) * 80);
+    sale = ((price / 100) * 20);
+    document.getElementById('sale').innerHTML = 'In base ai dati forniti puoi beneficiare di uno sconto del 20% e risparmi ben: ' + sale.toFixed(2) + ' €!';
 } else if (age > 65){
     finalPrice = ((price / 100) * 60);
+    sale = ((price / 100) * 40);
+    document.getElementById('sale').innerHTML = 'In base ai dati forniti puoi beneficiare di uno sconto del 40% e risparmi ben: ' + sale.toFixed(2) + ' €!';
 } else {
     finalPrice = price;
 }
